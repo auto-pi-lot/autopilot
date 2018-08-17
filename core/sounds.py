f866ca55b2e7afd20953b21f2e36e3ffd029d19e
Jonny <JLSaunders987@gmail.com>
2018-08-17 00:21:50 -0700

    reverting sound metaclass

--- core/sounds.py
+++ core/sounds.py
@@ -53,7 +53,7 @@ class Sound(object):
 
 
 
-class Tone(Sound):
+class Tone:
     '''
     The Humble Sine Wave
     '''
@@ -66,10 +66,18 @@ class Tone(Sound):
         self.amplitude = float(amplitude)
 
         sin = pyo.Sine(self.frequency, mul=self.amplitude)
-        self.table = self.table_wrap(sin, self.duration)
-    #
-    # def play(self):
-    #     self.table.out()
+        self.table = TableWrap(sin, self.duration)
+
+
+
+
+    def set_trigger(self, trig_fn):
+        # TODO: Put this in metaclass
+        # Using table triggers...
+        self.trigger = pyo.TrigFunc(self.table['trig'], trig_fn)
+
+    def play(self):
+        self.table.out()
 
 
 class Noise(Sound):
@@ -81,14 +89,14 @@ class Noise(Sound):
         super(Noise, self).__init__()
 
         noiser = pyo.Noise(mul=float(amplitude))
-        self.table = self.table_wrap(noiser,float(duration))
+        self.table = TableWrap(noiser,float(duration))
 
-    # def play(self):
-    #     self.table.out()
-    #
-    # def set_trigger(self, trig_fn):
-    #     # Using table triggers...
-    #     self.trigger = pyo.TrigFunc(self.table['trig'], trig_fn)
+    def play(self):
+        self.table.out()
+
+    def set_trigger(self, trig_fn):
+        # Using table triggers...
+        self.trigger = pyo.TrigFunc(self.table['trig'], trig_fn)
 
 class File(Sound):
     PARAMS = ['path', 'amplitude']
@@ -104,9 +112,26 @@ class File(Sound):
         self.table = pyo.TableRead(self.snd_table, freq=self.snd_table.getRate(),
                                    loop=False, mul=self.amplitude)
 
-    # def play(self):
-    #     # test
-    #     pass
+
+
+    def play(self):
+        self.table.out()
+
+def TableWrap(audio,duration):
+    '''
+    Records a PyoAudio generator into a sound table, returns a tableread object which can play the audio with .out()
+    '''
+    # Duration is in ms, so divide by 1000
+    # See https://groups.google.com/forum/#!topic/pyo-discuss/N-pan7wPF-o
+    #TODO: Get chnls to be responsive to NCHANNELS in prefs. hardcoded for now
+    #audio.play()
+    tab = pyo.NewTable(length=(float(duration)/1000),chnls=2) # Prefs should always be declared in the global namespace
+    tabrec = pyo.TableRec(audio,table=tab,fadetime=0.01).play()
+    sleep((float(duration)/1000))
+    tabread = pyo.TableRead(tab,freq=tab.getRate(), loop=0)
+    #audio.stop()
+    #tabrec.stop()
+    return tabread
 
 
 # Has to be at bottom so fnxns already defined when assigned.
