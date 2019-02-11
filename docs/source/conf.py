77e3547f8086211c118de71ad8a2cce48286885e
sneakers-the-rat <JLSaunders987@gmail.com>
2019-02-11 13:54:26 -0800

    getting rtd to render

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,7 +72,7 @@ napoleon_include_special_with_doc = True
 autoclass_content = "both"
 autodoc_member_order = "bysource"
 #autodoc_default_flags = ['members']
-autodoc_mock_imports = ['tables', 'PySide', 'tables.nodes']
+autodoc_mock_imports = ['tables', 'PySide', 'tables.nodes', 'pyo']
 
 automodsumm_writereprocessed = True
 
