822c5065ab63efcd9efde38ad3b68490fa16129a
sneakers-the-rat <JLSaunders987@gmail.com>
2019-09-13 17:44:38 -0700

    guide progress, adding visual setup to pilot setup

--- rpilot/stim/__init__.py
+++ rpilot/stim/__init__.py
@@ -1,2 +1,6 @@
+from rpilot import prefs
+
 from rpilot.stim.managers import Stim_Manager, Proportional, init_manager
-from rpilot.stim.sound import sounds
+
+if prefs.CONFIG == "AUDIO":
+    from rpilot.stim.sound import sounds
