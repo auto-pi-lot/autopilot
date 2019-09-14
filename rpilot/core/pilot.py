372c1ff7ebc5aaa53b58685856d1cc114fea4113
sneakers-the-rat <JLSaunders987@gmail.com>
2019-09-13 17:39:02 -0700

    guide progress, adding visual setup to pilot setup

--- rpilot/core/pilot.py
+++ rpilot/core/pilot.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     prefs.init(prefs_file)
     sys.path.append(os.path.dirname(prefs.REPODIR))
 
-    if hasattr(prefs, 'AUDIOSERVER'):
+    if hasattr(prefs, 'AUDIOSERVER') and prefs.CONFIG == 'AUDIO':
         if prefs.AUDIOSERVER == 'pyo':
             from rpilot.stim.sound import pyoserver
         elif prefs.AUDIOSERVER == 'jack':
@@ -160,7 +160,7 @@ class RPilot:
         self.file_block = threading.Event() # Are we waiting on file transfer?
 
         # Init audio server
-        if hasattr(prefs, 'AUDIOSERVER'):
+        if hasattr(prefs, 'AUDIOSERVER') and prefs.CONFIG == 'AUDIO':
             self.init_audio()
 
         # Init Station
