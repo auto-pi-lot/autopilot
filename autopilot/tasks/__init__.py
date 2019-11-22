676cd07c0756ed91c7496552f3edde99352a2b73
sneakers-the-rat <JLSaunders987@gmail.com>
2019-11-22 12:42:45 -0800

    task list

--- autopilot/tasks/__init__.py
+++ autopilot/tasks/__init__.py
@@ -1,5 +1,5 @@
 from autopilot.tasks.task import Task
-from autopilot.tasks.nafc import Nafc
+from autopilot.tasks.nafc import Nafc, Nafc_Gap
 from autopilot.tasks.gonogo import GoNoGo
 from autopilot.tasks.free_water import Free_Water
 from autopilot.tasks.graduation import GRAD_LIST
@@ -7,8 +7,10 @@ from autopilot.tasks.children import Wheel_Child
 
 
 TASK_LIST = {'2AFC':Nafc,
+             '2AFC_Gap':Nafc_Gap,
              'Free Water':Free_Water,
-             'GoNoGo': GoNoGo}
+             'GoNoGo': GoNoGo,
+             }
              # unfinished tasks
              # '2AFC_Wheel': Nafc_Wheel,
              #'Gap 2AFC':Gap_2AFC}
