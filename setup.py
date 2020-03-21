3d88709888125975783eb720c6ea04104299979d
sneakers-the-rat <jlsaunders987@gmail.com>
2020-03-20 18:34:25 -0700

    minor cleanup setup.py, ignoring blank or commented requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 import subprocess
 subprocess.call('autopilot/setup/setup_environment.sh')
 
-import sys
 from skbuild import setup
 from setuptools import find_packages
 import subprocess
@@ -13,8 +12,6 @@ SCRIPTS = []
 PACKAGES = []
 CMAKE_ARGS = []
 REQUIREMENTS = []
-REQUIREMENTS_LINKS = []
-CMAKE_INSTALL_DIR = ''
 
 
 # detect if on raspberry pi
@@ -23,29 +20,32 @@ if ret == 0:
     IS_RASPI = True
 
 
+def load_requirements(req_file):
+    with open(req_file, 'r') as f:
+        requirements = f.read().splitlines()
+
+    # filter commented reqs
+    requirements = [r for r in requirements if not f.startswith('#')]
+
+    # filter empty reqs
+    requirements = [r for r in requirements if r]
+
+    return requirements
+
 # configure for raspberry pi
 if IS_RASPI:
     CMAKE_ARGS = ['-DPIGPIO=ON']
-    #CMAKE_INSTALL_DIR = '/usr/local'
     #SCRIPTS.append('autopilot/external/pigpio/pigpiod')
     PACKAGES.append('autopilot.external.pigpio')
-    with open('requirements_pilot.txt', 'r') as f:
-        REQUIREMENTS = f.read().splitlines()
+    REQUIREMENTS = load_requirements('requirements_pilot.txt')
+
 else:
     # is a terminal, should have minimum dependencies installed
     # sys.argv.append('--skip-cmake')
-    with open('requirements_terminal.txt', 'r') as f:
-        REQUIREMENTS = f.read().splitlines()
-
-for req in REQUIREMENTS:
-    if req.startswith('git+'):
-        REQUIREMENTS_LINKS.append(req)
-        REQUIREMENTS.remove(req)
-        # REQUIREMENTS.append(req.split('/')[-1].rstrip('.git'))
+    REQUIREMENTS = load_requirements('requirements_terminal.txt')
 
 
-print(REQUIREMENTS)
-
+# add external packages that wouldn't get detected normally
 packs = find_packages()
 packs.extend(PACKAGES)
 
@@ -56,12 +56,9 @@ setup(
     author="Jonny Saunders",
     author_email="JLSaunders987@gmail.com",
     url="https://auto-pi-lot.com",
-    license="MPL2",
+    license="MPL-2.0",
     scripts = SCRIPTS,
-    # dependency_links=['src/pigpio/'],
     packages=packs,
     cmake_args=CMAKE_ARGS,
-    cmake_install_dir = CMAKE_INSTALL_DIR,
-    install_requires = REQUIREMENTS,
-    dependency_links = REQUIREMENTS_LINKS
+    install_requires = REQUIREMENTS
 )
