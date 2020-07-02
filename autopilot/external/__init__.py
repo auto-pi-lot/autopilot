1f96b65764092d3b9f7c484970e6d9d8433b0010
sneakers-the-rat <JLSaunders987@gmail.com>
2020-07-01 17:50:04 -0700

    detecting whether using system jackd and making copacetic with _find_libraries from ctypes

--- autopilot/external/__init__.py
+++ autopilot/external/__init__.py
@@ -12,41 +12,44 @@ except ImportError:
     pass
 
 JACKD = False
-JACKD_MODULE = None # Whether jackd is a module in autopilot.external (True) or use system jackd (False)
+# JACKD_MODULE = None # Whether jackd is a module in autopilot.external (True) or use system jackd (False)
 JACKD_PROCESS = None
 try:
-    from autopilot.external import jack as autopilot_jack
-
-    # set env variables
-    jackd_path = os.path.join(autopilot_jack.__path__._path[0])
-
-    # specify location of libraries when starting jackd
-
-
-    if 'LD_LIBRARY_PATH' in os.environ.keys():
-
-        os.environ['LD_LIBRARY_PATH'] = ":".join([os.path.join(jackd_path, 'lib'),
-                                                  os.environ.get('LD_LIBRARY_PATH',"")])
-    else:
-        os.environ['LD_LIBRARY_PATH'] = os.path.join(jackd_path, 'lib')
-    # lib_string = "LD_LIBRARY_PATH=" + os.path.join(jackd_path, 'lib')
-
-    # specify location of drivers when starting jackd
-    os.environ['JACK_DRIVER_DIR'] = os.path.join(jackd_path, 'lib', 'jack')
-    # driver_string = "JACK_DRIVER_DIR=" + os.path.join(jackd_path, 'lib', 'jack')
-
+    import jack
     JACKD = True
-    JACKD_MODULE = True
+    # from autopilot.external import jack as autopilot_jack
+    #
+    # # set env variables
+    # jackd_path = os.path.join(autopilot_jack.__path__._path[0])
+    #
+    # # specify location of libraries when starting jackd
+    #
+    #
+    # if 'LD_LIBRARY_PATH' in os.environ.keys():
+    #
+    #     os.environ['LD_LIBRARY_PATH'] = ":".join([os.path.join(jackd_path, 'lib'),
+    #                                               os.environ.get('LD_LIBRARY_PATH',"")])
+    # else:
+    #     os.environ['LD_LIBRARY_PATH'] = os.path.join(jackd_path, 'lib')git pu
+    # # lib_string = "LD_LIBRARY_PATH=" + os.path.join(jackd_path, 'lib')
+    #
+    # # specify location of drivers when starting jackd
+    # os.environ['JACK_DRIVER_DIR'] = os.path.join(jackd_path, 'lib', 'jack')
+    # # driver_string = "JACK_DRIVER_DIR=" + os.path.join(jackd_path, 'lib', 'jack')
+    #
+    # JACKD = True
+    # JACKD_MODULE = True
 
-except ImportError:
-    # try to import jack client, it will look for system jack by default
-    try:
-        import jack
-        JACKD = True
-        JACKD_MODULE = False
-    except OSError:
-        # no need to do anything, just setting module variables that test what the system is configured to do
-        pass
+except (ImportError, OSError):
+    pass
+    # # try to import jack client, it will look for system jack by default
+    # try:
+    #     import jack
+    #     JACKD = True
+    #     JACKD_MODULE = False
+    # except OSError:
+    #     # no need to do anything, just setting module variables that test what the system is configured to do
+    #     pass
 
 
 def start_pigpiod():
@@ -62,7 +65,8 @@ def start_pigpiod():
             prefs.PIGPIOMASK = str(prefs.PIGPIOMASK).zfill(28)
         launch_pigpiod += ' -x ' + prefs.PIGPIOMASK
 
-    proc = subprocess.Popen('sudo ' + launch_pigpiod, shell=True)
+    proc = subprocess.Popen('sudo ' + launch_pigpiod, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,
+                            creationflags= subprocess.HIGH_PRIORITY_CLASS | subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP)
 
     # kill process when session ends
     atexit.register(lambda pigpio_proc=proc: pigpio_proc.kill())
@@ -84,30 +88,31 @@ def start_jackd():
         jackd_string = ""
 
     # construct rest of launch string!
-    if JACKD_MODULE:
-        jackd_path = os.path.join(autopilot_jack.__path__._path[0])
-
-        # now set as env variables...
-        # specify location of libraries when starting jackd
-        # lib_string = "LD_LIBRARY_PATH=" + os.path.join(jackd_path, 'lib')
-        #
-        # specify location of drivers when starting jackd
-        # os.environ['JACK_DRIVER_DIR'] = os.path.join(jackd_path, 'lib', 'jack')
-        # driver_string = "JACK_DRIVER_DIR=" + os.path.join(jackd_path, 'lib', 'jack')
-
-        jackd_bin = os.path.join(jackd_path, 'bin', 'jackd')
-
-        # combine all the pieces
-        # launch_jackd = " ".join([lib_string, driver_string, jackd_bin, jackd_string])
-
-    else:
-        jackd_bin = 'jackd'
+    # if JACKD_MODULE:
+    #     jackd_path = os.path.join(autopilot_jack.__path__._path[0])
+    #
+    #     # now set as env variables...
+    #     # specify location of libraries when starting jackd
+    #     # lib_string = "LD_LIBRARY_PATH=" + os.path.join(jackd_path, 'lib')
+    #     #
+    #     # specify location of drivers when starting jackd
+    #     # os.environ['JACK_DRIVER_DIR'] = os.path.join(jackd_path, 'lib', 'jack')
+    #     # driver_string = "JACK_DRIVER_DIR=" + os.path.join(jackd_path, 'lib', 'jack')
+    #
+    #     jackd_bin = os.path.join(jackd_path, 'bin', 'jackd')
+    #
+    #     # combine all the pieces
+    #     # launch_jackd = " ".join([lib_string, driver_string, jackd_bin, jackd_string])
+    #
+    # else:
+    jackd_bin = 'jackd'
 
         # launch_jackd = " ".join([jackd_bin, jackd_string])
 
     launch_jackd = " ".join([jackd_bin, jackd_string])
 
-    proc = subprocess.Popen(launch_jackd, shell=True)
+    proc = subprocess.Popen(launch_jackd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,
+                            creationflags= subprocess.REALTIME_PRIORITY_CLASS | subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP)
     globals()['JACKD_PROCESS'] = proc
 
     # kill process when session ends
