934aa5008c7866f0004fcda2bce490bacf46ae28
sneakers-the-rat <JLSaunders987@gmail.com>
2020-07-16 20:33:54 -0700

    catching all trigs

--- autopilot/tasks/test.py
+++ autopilot/tasks/test.py
@@ -185,7 +185,7 @@ class DLC_Latency(Task):
             'child': {'parent': prefs.NAME, 'subject': kwargs['subject']},
             'task_type': 'Transformer',
             'subject': kwargs['subject'],
-            'operation':'trigger',
+            'operation':'stream',
             'transform':transform_descriptor,
             'return_id': self.node_id
         }
@@ -270,6 +270,7 @@ class DLC_Latency(Task):
     def l_trigger(self, value):
         if value:
             self.handle_trigger(pin="CAMERA")
+            self.hardware['DIGITAL_OUT']['C'].pulse
 
 
 
