0aa70b4d3ebd1233bdd022dd4a91a54f35ce3ffe
sneakers-the-rat <JLSaunders987@gmail.com>
2020-07-19 11:52:15 -0700

    import errors

--- autopilot/tasks/__init__.py
+++ autopilot/tasks/__init__.py
@@ -5,7 +5,7 @@ from autopilot.tasks.parallax import Parallax
 from autopilot.tasks.free_water import Free_Water
 from autopilot.tasks.graduation import GRAD_LIST
 from autopilot.tasks.children import Wheel_Child, Video_Child, Transformer
-from autopilot.tasks.test import DLC_Latency
+from autopilot.tasks.test import DLC_Latency, DLC_Hand
 
 
 TASK_LIST = {'2AFC':Nafc,
@@ -13,7 +13,8 @@ TASK_LIST = {'2AFC':Nafc,
              'Free Water':Free_Water,
              'GoNoGo': GoNoGo,
              'Parallax': Parallax,
-             'Test_DLC_Latency': DLC_Latency}
+             'Test_DLC_Latency': DLC_Latency,
+             'Test_DLC_Hand':DLC_Hand}
 """
 Link between string task names used in protocol descriptions and task classes
 """
