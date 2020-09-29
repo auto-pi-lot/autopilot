a3d3e3f11cf32a9ffc1458823cba6da2307861d7
sneakers-the-rat <JLSaunders987@gmail.com>
2020-09-29 00:01:14 -0700

    v0.3.2 changelog documentation

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ with open(os.path.join(
 
 setup(
     name="auto-pi-lot",
-    version="0.3.1",
+    version="0.3.2",
     description="Distributed behavioral experiments",
     long_description = readme,
     long_description_content_type='text/markdown',
