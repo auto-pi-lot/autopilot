30d3d5db1dccc5a644ce91236f3091ce39c8ede1
sneakers-the-rat <JLSaunders987@gmail.com>
2020-12-01 23:39:07 -0800

    whups

--- tests/test_prefs.py
+++ tests/test_prefs.py
@@ -8,7 +8,7 @@ def test_prefs_defaults(default_pref):
     # make sure that we didnt' actually load anything from some phantom uh prefs file idk
     assert not prefs._INITIALIZED.value
 
-    if hasattr(default_pref[1], 'default'):
+    if 'default' in default_pref[1].keys():
         assert prefs.get(default_pref[0]) == default_pref[1]['default']
     else:
         assert prefs.get(default_pref[0]) is None
