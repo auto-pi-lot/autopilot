eca4c06de03500b30bab7ffabad9e86c2bcafd81
Mike Wehr <wehr@uoregon.edu>
2020-12-08 10:11:48 -0800

    Update __init__.py
    added some concrete examples to the GPIO numbering documentation

--- autopilot/hardware/__init__.py
+++ autopilot/hardware/__init__.py
@@ -18,8 +18,8 @@ very few design requirements:
 Note:
     This software was primarily developed for the Raspberry Pi, which
     has `two types of numbering schemes <https://pinout.xyz/#>`_ ,
-    "board" numbering based on physical position and "bcm" numbering
-    based on the broadcom chip numbering scheme.
+    "board" numbering based on physical position (e.g. pins 1-40, in 2 rows of 20 pins) and "bcm" numbering
+    based on the broadcom chip numbering scheme (e.g. GPIO2, GPIO27).
 
     Board numbering is easier to use, but `pigpio <http://abyz.me.uk/rpi/pigpio/>`_
     , which we use as a bridge between Python and the GPIOs, uses the BCM scheme.
