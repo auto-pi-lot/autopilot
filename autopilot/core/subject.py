628e1fb9c8fcd15399b19b351fed87e4826bc9ab
sneakers-the-rat <JLSaunders987@gmail.com>
2020-12-22 17:06:29 -0800

    provisionally allow dataless tasks...

--- autopilot/core/subject.py
+++ autopilot/core/subject.py
@@ -681,8 +681,8 @@ class Subject(object):
                 session_group = h5f.get_node(cont_group, "session_{}".format(self.session))
             # don't create arrays for each dtype here, we will create them as we receive data
 
-        if (trial_table is None) and (cont_group is None):
-            raise Exception("No data tables exist for step {}! Is there a Trial or Continuous data descriptor in the task class?".format(self.step))
+        # if (trial_table is None) and (cont_group is None):
+        #     raise Exception("No data tables exist for step {}! Is there a Trial or Continuous data descriptor in the task class?".format(self.step))
 
         # TODO: Spawn graduation checking object!
         self.graduation = None
