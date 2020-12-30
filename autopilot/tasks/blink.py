ba30b68c691ea0637901cde6557debb02e7103bd
Mike Wehr <wehr@uoregon.edu>
2020-12-29 18:10:04 -0800

    Update blink.py
    adding documentation

--- autopilot/tasks/blink.py
+++ autopilot/tasks/blink.py
@@ -27,21 +27,31 @@ class Blink(Task):
 	# Just blink an LED
 
 
-	STAGE_NAMES = ["pulse"]
+	STAGE_NAMES = ["pulse"] 
+	#there's only one stage, which consists of a single LED flash
 
 
 	PARAMS = odict()
-	PARAMS['pulse_duration']         = {'tag':'LED Pulse Duration (ms)',
-	                            'type':'int'}
-	PARAMS['pulse_interval']         = {'tag':'LED Pulse Interval (ms)',
-	                            'type':'int'}
+	PARAMS['pulse_duration']         = {'tag':'LED Pulse Duration (ms)', 'type':'int'}
+	PARAMS['pulse_interval']         = {'tag':'LED Pulse Interval (ms)', 'type':'int'}
+
 	class TrialData(tables.IsDescription):
-	        # This class allows the Subject object to make a data table with the correct data types. You must update it for any new data you'd like to store
+	        """This class allows the Subject object to make a data table with the
+			correct data types. You must update it for any new data you'd like to store
+			For a blinking LED there isn't much in the way of data, but we (probably) need
+			to return at least something  """
 	        trial_num = tables.Int32Col()
 
+	"""the only hardware here is a digital out to flash the LED. Here we name it dLED.
+	when you setup autopilot on the raspberry pi, include a GPIO Digital Out and
+	specify the pin #. Then connect your LED (with a current limiting resistor,
+	e.g. 10 ohm) to that pin. For example if you specify pin 40, use the
+	bottom-right pin also known as GPIO#21. Note that we create dLED inside of a
+	group (not sure why) and the prefs.json on the pi has to have the same group
+	structure (dLED inside of LEDS) """
 	HARDWARE = {
-	 	'LEDS':{ #dLED has to be inside of a group for some reason
-	       'dLED': gpio.Digital_Out
+		'LEDS':{ 
+	       'dLED': gpio.Digital_Out 
         }
 	}
 
@@ -52,8 +62,8 @@ class Blink(Task):
 		self.pulse_duration = int(pulse_duration)
 		self.pulse_interval = int(pulse_interval)
 
-	# This allows us to cycle through the task by just repeatedly calling self.stages.next()
-		stage_list = [self.pulse]
+		# This allows us to cycle through the task by just repeatedly calling self.stages.next()
+		stage_list = [self.pulse] #a list of only one stage, the pulse
 		self.num_stages = len(stage_list)
 		self.stages = itertools.cycle(stage_list)
 		self.trial_counter = itertools.count()
@@ -62,10 +72,8 @@ class Blink(Task):
 		self.init_hardware()
 		self.logger.debug('Hardware initialized')
 
-		self.hardware['LEDS']['dLED'].set(1)
-		self.logger.debug('test message')
-
 		self.stage_block = stage_block
+		#this is the threading.event object that is used to advance from one stage to the next 
 
 
 
@@ -79,21 +87,22 @@ class Blink(Task):
 		"""
 
 		self.hardware['LEDS']['dLED'].set(1)
-		self.logger.debug('light on')
+		#self.logger.debug('light on')
 		time.sleep(self.pulse_duration / 1000)
 
 		self.hardware['LEDS']['dLED'].set(0)
-		self.logger.debug('light off')
+		#self.logger.debug('light off')
 		time.sleep(self.pulse_interval / 1000)
 
 
 		self.current_trial = next(self.trial_counter)
 		self.current_stage = 0
-		self.logger.debug(f'trial {self.current_trial}')
+		#self.logger.debug(f'trial {self.current_trial}')
 
-#		next(self.stages)
 		self.stage_block.set()
+		#this clears the stage block so we advance to the next stage 
 
+		#return the trial number as data
 		data = {'trial_num' : self.current_trial}
 		return data
 
