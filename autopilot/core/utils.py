066d7636c6c0503bc32fbfa5f7203ff241fb27c0
sneakers-the-rat <j@nny.fyi>
2021-01-21 14:50:35 -0900

    bugfix utils.py mike

--- autopilot/core/utils.py
+++ autopilot/core/utils.py
@@ -229,7 +229,7 @@ def find_recursive(key, dictionary):
     Returns:
         list
     """
-    for k, v in dictionary.iteritems():
+    for k, v in dictionary.items():
         if k == key:
             yield v
         elif isinstance(v, dict):
