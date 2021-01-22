3af550e814846431d281252764282b315a5f562a
Mike Wehr <wehr@uoregon.edu>
2021-01-21 16:28:25 -0800

    Update nafc.py
    fixing np.max not working on list of str

--- autopilot/tasks/nafc.py
+++ autopilot/tasks/nafc.py
@@ -599,9 +599,10 @@ class Nafc_Gap_Laser(Nafc_Gap):
             self.hardware['LEDS']['TOP'].turn(True)
         elif self.arena_led_mode == "STIM":
             stim_durations = list(find_recursive('duration', kwargs['stim']))
-            import pdb
-            pdb.set_trace()
-            max_duration = np.max(stim_durations)
+            stim_durations_int = [int(i) for i in stim_durations]
+            #import pdb
+            #pdb.set_trace()
+            max_duration = np.max(stim_durations_int)
             self.hardware['LEDS']['TOP'].store_series('on', values=1, durations=max_duration )
         else:
             raise ValueError(f'arena_led_mode must be one of ON or STIM, got {self.arena_led_mode}')
