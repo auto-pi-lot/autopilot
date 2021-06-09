12f2c915f36081f7755a78f828701862f11b2688
Chris Rodgers <xrodgers@gmail.com>
2021-06-09 11:10:40 -0400

    adding test_sound.py

---
+++ tests/test_sound.py
@@ -0,0 +1,11 @@
+import autopilot.prefs
+import autopilot.stim.sound
+
+
+def test_init_noise():
+    # Initialize Noise
+    noise = autopilot.stim.sound.sounds.Noise(duration=100)
+    
+    # Test
+
+test_init_noise()
