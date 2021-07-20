3c88919dfbffca0debef9b4a67c43d95c942ba2a
sneakers-the-rat <JLSaunders987@gmail.com>
2021-07-20 13:18:50 -0700

    task and hardware aliases

--- autopilot/utils/registry.py
+++ autopilot/utils/registry.py
@@ -7,13 +7,17 @@ be integrated across the system.
 
 """
 from enum import Enum
-from typing import Union
 import multiprocessing as mp
+from typing import TYPE_CHECKING, Union, Optional, List
+
 
 from autopilot.utils.common import find_class, recurse_subclasses, list_classes
 from autopilot.utils.plugins import import_plugins, _IMPORTED
 from autopilot import prefs
 from autopilot.core.loggers import init_logger
+if TYPE_CHECKING:
+    from autopilot.hardware import Hardware
+    from autopilot.tasks import Task
 
 _IMPORT_LOCK = mp.Lock()
 """
@@ -33,7 +37,7 @@ class REGISTRIES(str, Enum):
     TASK = "autopilot.tasks.Task"
 
 # TODO: Update return type hint when unified Autopilot Object class made
-def get(base_class:Union[REGISTRIES,str], class_name:str, plugins:bool=True, ast:bool=True):
+def get(base_class:Union[REGISTRIES,str], class_name:Optional[str]=None, plugins:bool=True, ast:bool=True) -> Union[Task, Hardware, List[Task], List[Hardware]]:
     """
     Get an autopilot object.
 
@@ -41,7 +45,8 @@ def get(base_class:Union[REGISTRIES,str], class_name:str, plugins:bool=True, ast
         base_class (:class:`.REGISTRIES`): Class to search its subclasses for the indicated object. One of the values in the :class:`.REGISTRIES` enum,
            or else one of its keys (eg. ``'HARDWARE'``). If given a full module.ClassName string
            (eg. ``"autopilot.tasks.Task"``) attempt to get the indicated object
-       class_name (str): Name of class that inherits from base_class.
+       class_name (str, None): Name of class that inherits from base_class that is to be returned.
+           if ``None`` (default), return all found subclasses of ``base_class``
        plugins (bool): If ``True`` (default), ensure contents of PLUGINDIR are loaded (with :func:`~.utils.plugins.import_plugins`)
            and are included in results. If ``False``, plugins are not explicitly imported, but if any
            have been imported elsewhere, they will be included anyway because we can't control all the
@@ -53,7 +58,7 @@ def get(base_class:Union[REGISTRIES,str], class_name:str, plugins:bool=True, ast
            (will miss any modules that aren't already imported).
 
     Returns:
-
+        Either the requested items, or a list of all the relevant items
     """
     logger = init_logger(module_name='registry', class_name='get')
 
@@ -83,26 +88,85 @@ def get(base_class:Union[REGISTRIES,str], class_name:str, plugins:bool=True, ast
     # find subclasses!
     subclasses = recurse_subclasses(cls)
     # if we have found one among the imported classes, return that
-    for subclass in subclasses:
-        if subclass.__name__ == class_name:
-            logger.debug(f"Found {subclass} as subclass of {cls} in module {subclass.__module__}")
-            return subclass
+    if class_name is not None:
+        for subclass in subclasses:
+            if subclass.__name__ == class_name:
+                logger.debug(f"Found {subclass} as subclass of {cls} in module {subclass.__module__}")
+                return subclass
 
     # if we haven't found the class yet and are asked to parse the ast tree, do it!
     if ast:
         ast_subclasses = list_classes(cls.__module__)
         for subclass in ast_subclasses:
-            if subclass[0] == class_name:
-                # import it and check that it is a subclass!
-                imported_subclass = find_class(subclass[1])
-                if issubclass(imported_subclass, cls):
-                    logger.debug(f"Found {imported_subclass} as subclass of {cls} in {subclass[1]}")
-                    return imported_subclass
+            # if we were asked to get a specific class...
+            if class_name is not None:
+                if subclass[0] == class_name:
+                    # import it and check that it is a subclass!
+                    imported_subclass = find_class(subclass[1])
+                    if issubclass(imported_subclass, cls):
+                        logger.debug(f"Found {imported_subclass} as subclass of {cls} in {subclass[1]}")
+                        return imported_subclass
+            else:
+                # get and import everything!
+                try:
+                    imported_subclass = find_class(subclass[1])
+                    if issubclass(imported_subclass, cls):
+                        subclasses.append(imported_subclass)
+                except ImportError as e:
+                    logger.warning(f'Exception importing requested class {subclass[1]}, got exception:\n{e}')
+
+    # if all the found classes were requested, return them.
+    if class_name is None:
+        return sorted(subclasses)
 
     # if we've gotten this far then we haven't found it :(
     ex_text = f"Could not find subclass of {cls} with name {class_name}!"
     logger.exception(ex_text)
     raise ValueError(ex_text)
+
+
+def get_hardware(class_name:Optional[str] = None, plugins:bool = True, ast:bool = True) -> Union[Hardware, List[Hardware]]:
+    """
+    Get a hardware class by name.
+
+    Alias for :func:`.registry.get`
+
+    Args:
+        class_name (str): Name of hardware class to get
+        plugins (bool): If ``True`` (default) ensure plugins are loaded and return
+            from them. see :func:`.registry.get` for more details about the
+            behavior of this argument
+        ast (bool): If ``True`` (default) parse the syntax tree of all modules
+            within :mod:`~autopilot.hardware`. see :func:`.registry.get` for more details about the
+            behavior of this argument
+
+    Returns:
+        :class:`~autopilot.hardware.Hardware`
+    """
+    return get(REGISTRIES.HARDWARE, class_name=class_name, plugins=plugins, ast=ast)
+
+
+def get_task(class_name:Optional[str] = None, plugins:bool = True, ast:bool = True) -> Union[Task, List[Task]]:
+    """
+    Get a task class by name.
+
+    Alias for :func:`.registry.get`
+
+    Args:
+        class_name (str): Name of task class to get
+        plugins (bool): If ``True`` (default) ensure plugins are loaded and return
+            from them. see :func:`.registry.get` for more details about the
+            behavior of this argument
+        ast (bool): If ``True`` (default) parse the syntax tree of all modules
+            within :mod:`~autopilot.tasks`. see :func:`.registry.get` for more details about the
+            behavior of this argument
+
+    Returns:
+        :class:`~autopilot.tasks.Task`
+    """
+    return get(REGISTRIES.TASK, class_name=class_name, plugins=plugins, ast=ast)
+
+
 
 
 
