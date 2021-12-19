871ff975d1be0b4231b3e121240588fb79829a83
sneakers-the-rat <JLSaunders987@gmail.com>
2021-12-18 17:57:18 -0800

    poetry conversion

--- autopilot/__init__.py
+++ autopilot/__init__.py
@@ -1,5 +1,13 @@
 __author__  = 'Jonny Saunders <j@nny.fyi>'
-__version__ = '0.4.3'
+
+import sys
+if sys.version_info.minor<8:
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
+
+__version__ = version("auto-pi-lot")
+
 
 from autopilot.setup import setup_autopilot
 from autopilot.utils.registry import get, get_task, get_hardware, get_names
