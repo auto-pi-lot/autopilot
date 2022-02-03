e408c08e76df8d8c930edfb4ba58e31cd1ec4d87
sneakers-the-rat <JLSaunders987@gmail.com>
2022-02-02 17:40:08 -0800

    [skip travis] freshing up the readme, adding python versions to setuptools metadata

--- setup.py
+++ setup.py
@@ -85,6 +85,8 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
