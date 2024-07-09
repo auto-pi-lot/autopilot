c9bf65980fb40a2c846ab0cd5a4f893cc6c2f1e6
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-07-09 15:55:39 -0700

    try mocker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,33 @@
 import pytest
 import os
 
-@pytest.fixture(scope='session', autouse=True)
-def monkeypatch_cpuinfo(monkeypatch):
+def on_gh_actions() -> bool:
+    return "CI" in os.environ or os.environ["CI"] or "GITHUB_RUN_ID" in os.environ
+
+@pytest.fixture(autouse=True)
+def monkeypatch_cpuinfo(mocker):
     # only patch on github actions
-    if "CI" not in os.environ or not os.environ["CI"] or "GITHUB_RUN_ID" not in os.environ:
-        return
+    # if not on_gh_actions():
+    #     return
 
-    def patched_cpuinfo() -> dict:
-        return {}
+    # def patched_cpuinfo() -> dict:
+    #     return {}
+    #
+    # class PatchedModule():
+    #     get_cpu_info = patched_cpuinfo
+    #
+    # import cpuinfo
+    # monkeypatch.setattr(cpuinfo, 'get_cpu_info', patched_cpuinfo)
+    #
+    # import blosc2.core
+    # monkeypatch.setattr(blosc2.core, 'get_cpu_info', patched_cpuinfo)
+    #
+    # import tables.leaf
+    # monkeypatch.setattr(tables.leaf, 'cpuinfo', PatchedModule)
 
-    import cpuinfo
-    monkeypatch.setattr(cpuinfo, 'get_cpu_info', patched_cpuinfo)
-
-    import blosc2.core
-    monkeypatch.setattr(blosc2.core, 'get_cpu_info', patched_cpuinfo)
-
+    mocker.patch('cpuinfo.get_cpu_info', return_value={})
+    mocker.patch('blosc2.core.get_cpu_info', return_value={})
+    mocker.patch('tables.leaf.cpuinfo.get_cpu_info', return_value={})
 
 
 
